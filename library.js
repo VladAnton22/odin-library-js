2982a765164bf0fc5b54687ea201b542999658ed
Vlad Anton <182820291+VladAnton22@users.noreply.github.com>
2025-05-20 18:04:57 +0100

    Made the form functional and added remove and change read status buttons

--- library.js
+++ library.js
@@ -12,12 +12,24 @@ function Book(title, author, pages, read) {
     }
 }
 
+Book.prototype.toggleReadStatus = function() {
+    if (this.read === false) {
+        this.read = true;
+    } else if (this.read === true) {
+        this.read = false;
+    }
+    displayBooks(myLibrary);
+}
+
 function addBookToLibrary(title, author, pages, read) {
     const book = new Book(title, author, pages, read);
     myLibrary.push(book);
 }
 
 function displayBooks(lib) {
+    const container = document.getElementById('library');
+    container.innerHTML = '';
+    
     for (const book of lib) {
         let bookCard = document.createElement('div');
         bookCard.className = 'book-card';
@@ -34,15 +46,62 @@ function displayBooks(lib) {
         let readStatus = document.createElement('p');
         readStatus.textContent = book.read ? "Read" : "Not read yet";
 
+        let removeBtn = document.createElement('button');
+        removeBtn.textContent = "Remove";
+        removeBtn.onclick = () => {
+            removeBook(book.id);
+        }
+
+        let readBtn = document.createElement('button');
+        readBtn.textContent = "Read";
+        readBtn.onclick = () => {
+            book.toggleReadStatus();
+        }
+        
         bookCard.appendChild(title);
         bookCard.appendChild(author);
         bookCard.appendChild(pages);
         bookCard.appendChild(readStatus);
+        bookCard.appendChild(removeBtn);
+        bookCard.appendChild(readBtn)
 
-        document.getElementById('library').appendChild(bookCard);
+        container.appendChild(bookCard);
     }
 }
 
-addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, false);
+function createBookForm(event) {
+    event.preventDefault();
 
-displayBooks(myLibrary);
+    const formData = new FormData(form);
+    const title = formData.get('title');
+    const author = formData.get('author');
+    const pages = parseInt(formData.get('pages'));
+    const read = formData.get('read') === 'on';
+
+    addBookToLibrary(title, author, pages, read);
+    displayBooks(myLibrary);
+
+    form.reset();
+    modal.close();
+}
+
+function removeBook(id) {
+    for (let i = 0; i < myLibrary.length; i++) {
+        if (myLibrary[i].id === id) {
+            myLibrary.splice(i, 1);
+            displayBooks(myLibrary);
+            break;
+        }
+    }
+}
+
+const modal = document.getElementById('bookModal');
+const openBtn = document.getElementById('openModal');
+const cancelBtn = document.getElementById('cancelBtn');
+
+openBtn.onclick = () => modal.showModal();
+cancelBtn.onclick = () => modal.close();
+
+const form = document.getElementById('bookForm');
+
+form.addEventListener('submit', createBookForm, false);
